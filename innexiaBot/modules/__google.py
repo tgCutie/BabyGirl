366ad99a95df3a59a9aa38e8d680be1f8979dd14
tgCutie <91736317+tgCutie@users.noreply.github.com>
2021-10-15 12:37:55 +0530

    Update __google.py

--- innexiaBot/modules/__google.py
+++ innexiaBot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> @Babygirl_robot  <==="
+        app_details += "\n\n===> @TheBabyGirl_robot  <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")

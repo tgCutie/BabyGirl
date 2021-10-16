6d23ce275b3c71e133e8c088cf404500376f904c
tgCutie <91736317+tgCutie@users.noreply.github.com>
2021-10-16 22:10:38 +0530

    Update __aimultilanguage.py

--- innexiaBot/modules/__aimultilanguage.py
+++ innexiaBot/modules/__aimultilanguage.py
@@ -59,20 +59,20 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("innexia AI Already Activated In This Chat")
+            await lel.edit("AI ChatBot Added but Something is Happening Wrong!\nContact in Support Group!")
             return
         await lel.edit(
-            f"innexia AI Successfully Added For Users In The Chat {message.chat.id}"
+            f"AI ChatBot Added but Something is Happening Wrong!\nContact in Support Group! {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("innexia AI Was Not Activated In This Chat")
+            await lel.edit("AI Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"innexia AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+            f"AI Successfully Deactivated For Users In The Chat {message.chat.id}"
         )
 
     elif status == "EN" or status == "en" or status == "english":
@@ -355,9 +355,8 @@ async def inuka(client, message):
 
 
 __help__ = """
-<b> Chatbot </b>
-innexia AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
- - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
+<b> Chatbot </b>UseLess Cmd!
+M - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
  
 """

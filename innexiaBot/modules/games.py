031fa2c087f9dafde3af6c5625239d60679ca899
tgCutie <91736317+tgCutie@users.noreply.github.com>
2021-10-22 07:55:44 +0530

    Update games.py

--- innexiaBot/modules/games.py
+++ innexiaBot/modules/games.py
@@ -62,7 +62,24 @@ async def _(event):
         except BaseException:
             pass
 
-
+@register(pattern="^/start(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    r = await event.reply(file=InputMediaDice("💕"))
+    input_int = int(input_str)
+    if input_int > 5:
+        await event.reply("How are you?")
+    
+    else:
+        try:
+            required_number = input_int
+            while r.media.value != required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice("💞"))
+        except BaseException:
+            pass
 
 __help__ = """
  *Play Game With Emojis:*

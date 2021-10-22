dcf08094e1a5482cf6967c4f7a7e32097ef7a2d5
tgCutie <91736317+tgCutie@users.noreply.github.com>
2021-10-22 08:05:04 +0530

    Update shippering.py

--- innexiaBot/modules/shippering.py
+++ innexiaBot/modules/shippering.py
@@ -46,7 +46,7 @@ async def couple(_, message):
             c2_mention = (await app.get_users(c2_id)).mention
 
             couple_selection_message = f"""**Couple of the day:**
-{c1_mention} + {c2_mention} = ❤️
+{c1_mention} + {c2_mention} = True ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(
                 message.chat.id,
